4fa94d04d35846def1cd54f122746d2771220ff7
tsubasa mochida <mochida@heartorg.co.jp>
2016-09-12 12:23:59 +0900

    added is_dicom

---
+++ is_dicom.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+import sys
+param = sys.argv
+
+# https://github.com/darcymason/pydicom/blob/26e769b63a12eb3295588961ed22a81643aa9de2/pydicom/misc.py
+def is_dicom(file):
+    fp = open(file, 'rb')
+    preamble = fp.read(0x80)
+    magic = fp.read(4)
+    if magic == b"DICM":
+        return True
+    else:
+        return False
+
+if is_dicom(param[1]) :
+    print "dicom"
+else :
+    print "not dicom"
+
