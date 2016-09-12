29429a570860646b043e460d6c0c2aacd4e07b18
tsubasa mochida <mochida@heartorg.co.jp>
2016-09-12 15:22:19 +0900

    added stdout/stderr to convert.py

--- convert.py
+++ convert.py
@@ -6,5 +6,8 @@ import anonymize_dicom
 
 param = sys.argv
 
-
-anonymize_dicom.anonymize_file(param[1], param[2]);
+try:
+    anonymize_dicom.anonymize_file(param[1], param[2]);
+    sys.stdout.write('Success')
+except:
+    sys.stderr.write('Error occurred!')
